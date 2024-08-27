355a7e9555682d6a6679e33765999a0d16df2ff4
Michael <michaelcesartools@hotmail.com>
2024-08-26 22:22:36 -0300

    [FRONT-32] Página metadados de evento

--- src/app/(main)/event/[id]/page.tsx
+++ src/app/(main)/event/[id]/page.tsx
@@ -1,25 +1,48 @@
-import { Typography } from "@/components/typography";
 import { fetchEventDetail } from "@/actions/fetch-event-detail";
-import type { Event } from "@/types/event"; // Import the Event type
 import { Suspense } from "react";
+import EventDesc from "../_components/event-desc";
+import EventLoc from "../_components/event-loc";
+import AddTicket from "../_components/add-ticket";
+import UserAdm from "../_components/user-adm";
+import { MainHeroEvent } from "../../_components/main-hero-event";
 
 export default async function Event({ params }: { params: { id: string } }) {
   const { id } = params;
   const event = await fetchEventDetail(id);
+  console.log(event)
 
   return (
-    <main className="flex flex-col justify-center items-center w-full h-screen bg-background p-4">
+    <main className="flex flex-col justify-center items-center w-full min-h-screen bg-background mt-8 mb-8">
+      <div className=" flex flex-col w-full py-8 px-[120px] gap-4">
       {event && (
         <Suspense fallback={<div>Loading...</div>}>
-          <Typography fontWeight={"bold"}>{event.uuid}</Typography>
-          <Typography fontWeight={"medium"}>{event.name}</Typography>
-          <Typography fontWeight={"medium"}>{event.description}</Typography>
-          <Typography fontWeight={"medium"}>
-            {event.category_display}
-          </Typography>
-          <Typography fontWeight={"medium"}>{event.address.city}</Typography>
+          
+          <MainHeroEvent></MainHeroEvent>
+
+          <div className="flex gap-6 w-full">
+            <AddTicket type="Inteira" price={event.ticket_value} available={event.tickets_available} sold={event.tickets_sold} parcels="10"/>
+            <AddTicket type="Meia" price={event.half_ticket_value} available={event.half_tickets_available} sold={event.half_ticket_quantity} parcels="10"/>
+          </div>
+          
+          <EventDesc description={event.description} />
+
+          <EventLoc
+            city={event.address.city}
+            street={event.address.street}
+            number={event.address.number}
+            district={event.address.district}
+            state={event.address.state}
+            zip_code={event.address.zip_code}
+          />
+
+          <UserAdm 
+            event={event}
+            />
+          
         </Suspense>
       )}
+
+      </div>
     </main>
   );
 }
