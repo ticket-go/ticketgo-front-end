c3ffb92b15282acf9702b7a27ae546dcbe3a00fd
michaelcesar <michaelcesartools@hotmail.com>
2024-09-09 03:20:04 -0300

    partner component adjustments

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,18 +1,26 @@
 import { Typography } from "../typography";
-import { cn } from "@/lib/utils"; 
+import { cn } from "@/lib/utils";
 
 interface ButtonProps {
   text: string;
-  className?: string; 
+  className?: string;
+  textColor?: string;
+  fontWeight?: string;
+  onClick?: () => void; 
 }
 
-export function Button({ text, className }: ButtonProps) {
+export function Button({ text, className, textColor, fontWeight, onClick }: ButtonProps) {
   return (
     <button
       data-testid="event-card-button"
-      className={cn("w-full h-12 bg-[#E85AFF] hover:bg-purple/80 rounded-sm", className)} 
+      className={cn("w-full h-12 bg-[#E85AFF] hover:bg-purple/80 rounded-sm", className)}
+      onClick={onClick} 
     >
-      <Typography fontWeight="medium" variant="h6">
+      <Typography
+        fontWeight="medium"
+        variant="h6"
+        className={cn(textColor, fontWeight ? `font-${fontWeight}` : "")}
+      >
         {text}
       </Typography>
     </button>
--- src/components/partner/index.tsx
+++ src/components/partner/index.tsx
@@ -1,15 +1,29 @@
-import { Bold } from "lucide-react";
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Typography } from "../typography";
 import { Button } from "../button";
 
+export function Partner() {
+    const router = useRouter();
 
+    const handleRedirect = () => {
+        router.push('/register'); 
+    };
 
-export function Partner () {
     return (
-        <div className="flex items-center justify-center flex-col w-full h-[300px] bg-purple-gradient mt-8 mb-8">
-            <Typography variant={"h3"} fontWeight={"bold"}>Seja nosso parceiro</Typography>
-            <Typography variant={"h3"} fontWeight={"bold"}>Tenha seu evento em nossa plataforma!</Typography>
-            <Button text="Criar uma conta"></Button>
+        <div className="flex items-center justify-center flex-col w-full h-[300px] bg-purple-gradient mt-8 mb-8 gap-4">
+            <div className="flex flex-col items-center">
+                <Typography variant={"h3"} fontWeight={"bold"}>Seja nosso parceiro</Typography>
+                <Typography variant={"h3"} fontWeight={"bold"}>Tenha seu evento em nossa plataforma!</Typography>
+            </div>
+            <Button
+                text="Criar uma conta"
+                className="w-[30%] h-[60px] bg-white hover:bg-white hover:scale-105 transition"
+                textColor="text-purple"
+                fontWeight="bold"
+                onClick={handleRedirect} 
+            />
         </div>
-    )
+    );
 }
