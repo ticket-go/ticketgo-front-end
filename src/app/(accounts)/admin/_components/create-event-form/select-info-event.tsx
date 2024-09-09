3633382ce911a021de6471d1a68b2282cb85dc9f
michaelcesar <michaelcesartools@hotmail.com>
2024-09-08 21:38:53 -0300

    responsiveness adjustments for the component select-info-event

--- src/app/(accounts)/admin/_components/create-event-form/select-info-event.tsx
+++ src/app/(accounts)/admin/_components/create-event-form/select-info-event.tsx
@@ -35,11 +35,11 @@ export function SelectInfoEvent({
 }: SelectInfoEventProps) {
   return (
     <div className="flex flex-col gap-4 w-full h-fit">
-      <div className="flex items-center gap-2 w-full h-full">
+      <div className="flex flex-col mobile:flex-col tab-port:flex-row gap-4 w-full">
         <div className="flex flex-col gap-2 w-full">
           <Label className="text-lg font-medium">Selecione uma categoria</Label>
           <Select {...register("category")}>
-            <SelectTrigger className="h-14">
+            <SelectTrigger className="h-14 w-full"> 
               <SelectValue placeholder="Nenhuma categoria selecionada" />
             </SelectTrigger>
             <SelectContent>
@@ -57,7 +57,7 @@ export function SelectInfoEvent({
             Selecione o status do evento
           </Label>
           <Select {...register("status")}>
-            <SelectTrigger className="h-14">
+            <SelectTrigger className="h-14 w-full"> 
               <SelectValue placeholder="Nenhum status selecionado" />
             </SelectTrigger>
             <SelectContent>
@@ -68,14 +68,13 @@ export function SelectInfoEvent({
           {errors.status && <ErrorMessage error={errors.status.message} />}
         </div>
       </div>
-
       <div className="flex flex-col gap-2 w-full h-fit">
         <Label className="text-lg font-medium">
           Qual o endereço do seu evento?
         </Label>
-        <div className="flex items-center w-full gap-4">
+        <div className="flex flex-col mobile:flex-col tab-port:flex-row items-center w-full gap-4">
           <Select {...register("address")}>
-            <SelectTrigger className="h-14">
+            <SelectTrigger className="h-14 w-full"> 
               <SelectValue placeholder="Nenhum endereço selecionado" />
             </SelectTrigger>
             <SelectContent>
--- src/app/(accounts)/admin/page.tsx
+++ src/app/(accounts)/admin/page.tsx
@@ -5,7 +5,8 @@ export default async function Admin() {
   const addresses = await fetchAddresses();
 
   return (
-    <main className="flex flex-col justify-center items-center w-full min-h-screen bg-background px-40 py-10 mt-20 mobile:px-6 mobile:py-6 mobile:mt-12 ">
+    <main className="flex flex-col justify-center items-center w-full min-h-screen bg-background px-40 py-10 mt-20 mobile:px-6 mobile:py-6 mobile:mt-12 
+    tab-port:px-8 tab-port:py-6 tab-port:mt-12 ">
       <CreateEventForm address={addresses} />
     </main>
   );
